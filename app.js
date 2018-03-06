07b24af2bce2b33523fcf4afa6f85271e538866f
Paul Huang <paul_hzq@hotmail.com>
2018-03-05 23:05:45 -0500

    add username to socket

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-
+usernames = [];
 server.listen(8080);
 
 app.get('/', function (req, res) {
@@ -9,9 +9,31 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
+  socket.on('new user', function(data, callback){
+    if(usernames.indexOf(data) !=-1){
+      callback(false);
+    } else {
+      callback(true);
+      socket.username = data;
+      usernames.push(socket.username);
+      updateUsernames();
+    }
+  });
+
+  //Update usernames
+  function updateUsernames(){
+    socket.emit('usernames', usernames);
+  }
   // socket.emit('news', { hello: 'world' });
-  socket.on('send message', function (data) {
+  socket.on('send message', function(data) {
     console.log(data);
-    io.sockets.emit('new message', {msg: data});
+    socket.emit('new message', {msg: data, user: socket.username});
+  });
+
+  //Disconnect
+  socket.on('disconnect', function(data){
+    if(!socket.username) return;
+    usernames.splice(usernames.indexOf(socket.username), 1);
+    updateUsernames();
   });
 });
